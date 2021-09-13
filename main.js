5d24db0e5ef1f7c3567209f99ddab81bb4debc22
xiaohehe <1296522554@qq.com>
2021-09-13 17:37:56 +0800

    init

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
-const $addNav = $('.addNav')
-const $search = $('.search')
-const $navList = $('.navList')
-const $lastLi = $navList.find('li.last')
+const $siteList = $('.siteList')
+const $lastLi = $siteList.find('li.last')
 const x = localStorage.getItem('x')
 const xObject = JSON.parse(x)
+
+//  search
+$(search).on('click', (e) => {
+	const $inputValue = $('input').val()
+	if ($inputValue === '') {
+		e.preventDefault()
+		alert("请输入您要跳转的网址！");
+	}
+})
+
 const hashMap = xObject || [
-  {logo: 'A', url: 'https://www.acfun.cn'},
+  {logo: 'G', url: 'https://github.com/hec990'},
   {logo: 'B', url: 'https://www.bilibili.com'}
 ]
 const simplifyUrl = (url) => {
@@ -15,33 +23,34 @@ const simplifyUrl = (url) => {
     .replace(/\/.*/, '') // 删除 / 开头的内容
 }
 
-// search
-$($search).on('click', (e) => {
-	const $inputValue = $('input').val()
-	if ($inputValue === '') {
-		e.preventDefault()
-		alert("请输入您要跳转的网址！");
-	}
-})
-
 const render = () => {
-  $navList.find('li:not(.last)').remove()
+  $siteList.find('li:not(.last)').remove()
   hashMap.forEach((node, index) => {
     const $li = $(`<li>
-      <div class="nav">
+      <div class="site">
         <div class="logo">${node.logo}</div>
         <div class="link">${simplifyUrl(node.url)}</div>
+        <div class="close">
+          <svg class="icon">
+            <use xlink:href="#icon-close"></use>
+          </svg>
+        </div>
       </div>
     </li>`).insertBefore($lastLi)
     $li.on('click', () => {
       window.open(node.url)
+    })
+    $li.on('click', '.close', (e) => {
+      e.stopPropagation() // 阻止冒泡
+      hashMap.splice(index, 1)
+      render()
     })
   })
 }
 
 render()
 
-$('.addNav').on('click', () => {
+$('.addButton').on('click', () => {
   let url = window.prompt('请问你要添加的网址是啥？')
   if (url.indexOf('http') !== 0) {
     url = 'https://' + url
@@ -67,3 +76,4 @@ $(document).on('keypress', (e) => {
     }
   }
 })
+
